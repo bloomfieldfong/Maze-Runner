7dfb7a651704d697d4f911f798af08915de37544
bloomfieldfong <bloomfieldf24@gmail.com>
2017-05-12 16:23:29 -0600

    .

---
+++ Maze/Assets/Prefabs/Colision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CollisionEnter(Collider col){
+		if (col.gameObject.name == "Cube") {
+			Application.LoadLevel ("menu");
+			Destroy (col.gameObject);
+
+		}
+
+	}
+}
---
+++ Maze/Assets/Win.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+		
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name == "Player 1 1 (Clone)") {
+			Application.LoadLevel ("menu");
+			Destroy (col.gameObject);
+
+		}
+		
+	}
+
+}
